29e34223e6b0ee3b427ac9a21945ac9d70713148
Ben <benymng@gmail.com>
2020-08-30 18:46:40 -0600

    fixed problem

--- application.py
+++ application.py
@@ -1,8 +1,11 @@
 from flask import Flask, redirect, render_template, request
 
 
-app = Flask(__m__)
+app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run()
