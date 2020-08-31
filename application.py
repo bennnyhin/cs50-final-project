4c578d3e004fd5421f7aff5e63924701feb2d707
Ben <benymng@gmail.com>
2020-08-30 18:51:22 -0600

    added test.html

--- application.py
+++ application.py
@@ -3,9 +3,14 @@ from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+    if request.method == "GET":
+        return render_template("index.html")
+    else:
+        text = request.form.get("name")
+        return render_template("index.html", text=text)
+    
 
 if __name__ == "__main__":
     app.run()
